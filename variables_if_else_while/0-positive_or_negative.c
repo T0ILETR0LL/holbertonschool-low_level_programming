eba72f3f60d831fb5d96d25d8d278076a6414116
T0ILETR0LL <marianne.endozo@gmail.com>
2025-02-26 09:11:54 +0000

    positivenegative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -13,19 +13,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	if (n < 0)
 	{
-		printf("%i is negative\n", n);
+		printf("%i is negative\n",n);
 	}
 	if (n == 0)
 	{
-		printf("%i is zero\n", n);
+		printf("%i is zero\n",n);
 	}
 	if (n > 0)
 	{
-		printf("%i is positive\n", n);
+		printf("%i is positive\n",n);
 	}
-
 	return (0);
 }
