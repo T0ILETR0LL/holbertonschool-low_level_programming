9edab72f05e5df948fe77e05f4e817bdb2f55a32
T0ILETR0LL <marianne.endozo@gmail.com>
2025-02-27 07:47:48 +0000

    9comma

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - print 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char number;
+	char space;
+	char comma;
+
+	space=0x20;
+	comma=0x2C;
+	
+	for (number = '0'; number <= '9'; number++)
+	{
+		putchar(number);
+		if (number < '9')
+		{
+			putchar(comma);
+			putchar(space);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
