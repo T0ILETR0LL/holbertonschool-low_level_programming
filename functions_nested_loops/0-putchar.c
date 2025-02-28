e1da17ee2bf79eb477190cc6edc7c620279766d5
T0ILETR0LL <marianne.endozo@gmail.com>
2025-02-28 11:10:38 +0000

    header

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Print _putchar
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('a');
+	_putchar('\n');
 	return (0);
 }
