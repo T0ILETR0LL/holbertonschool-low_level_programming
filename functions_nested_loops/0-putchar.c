8ab9efd7202caa5ad815b43054f96156f5ba44be
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-03 07:24:00 +0000

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -5,7 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	char *str = "putchar";
 
@@ -14,7 +14,7 @@ int main (void)
 	{
 		_putchar(*str);
 		str++;
-	}	
+	}
 	_putchar('\n');
 	return (0);
 }
