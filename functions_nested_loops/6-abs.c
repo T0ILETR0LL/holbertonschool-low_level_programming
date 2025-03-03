5570834e3dd7703c6f240e54fd18bac3bcdc8f4c
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-03 12:04:31 +0000

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absoloute value of an interger
+ * @n: variable
+ * Return: (1) positive, (0) zero, (-1) negative, (-98) 
+ */
+int _abs(int n)
+{
+	
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else 
+	{
+		return (n);
+	}
+}
