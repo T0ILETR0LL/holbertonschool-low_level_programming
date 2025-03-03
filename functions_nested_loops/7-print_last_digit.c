7020667537c8af696f4fb55e514782cafd18249d
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-03 12:40:20 +0000

    last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ * @n: variable
+ * Return: (n)
+ */
+int print_last_digit(int n)
+{	
+	int last_n;
+	last_n = (n % 10);
+	_putchar (last_n);
+	return (n);
+
+}
