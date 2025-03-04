5bcea5099ca5b7ec4661e5915c3287714f44a643
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-04 06:42:18 +0000

    digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _ist - Checks for digit 0-9
+ * @c: variable
+ * Return: (1) if c is digit, (0) otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
