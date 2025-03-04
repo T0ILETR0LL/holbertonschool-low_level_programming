accaffd970d28afba877fa309d72732f047847c4
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-04 06:54:21 +0000

    multiply

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two intergers
+ * 
+ * Return: (1) if c is digit, (0) otherwise
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = (a*b);
+	return (result);
+	
+}
