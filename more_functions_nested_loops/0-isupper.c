50da630d83517fb5e678e15fa8234a6659eb79c9
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-04 06:39:09 +0000

    upper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper: checks for uppercase character
+ * _isupper - Checks for uppercase character
  * @c: variable
  * Return: (1) if c is uppercase, (0) otherwise
  */
