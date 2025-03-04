c0c12c1c8d363448ae8ab31b94839248e99fac05
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-04 08:22:57 +0000

    no24

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0-9, not including 2 and 4
+ *
+ * Return: (0)
+ */
+void print_most_numbers(void)
+{
+	char *number = "1356789";
+
+	while (*number != '\0')
+	{
+		_putchar(*number);
+		number++;
+	}
+	_putchar('\n');
+}
