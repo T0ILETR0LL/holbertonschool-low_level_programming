9b52120c2f083464aa1cc881ddef8d5eb8157cc6
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-04 09:05:47 +0000

    underscore

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character _ is printed
+ * Return: (0)
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		while (n > 0)
+		{
+			_putchar('_');
+			n--;
+		}
+		_putchar('\n');
+	}
+	else 
+	{
+		_putchar('\n');
+	}
+}
