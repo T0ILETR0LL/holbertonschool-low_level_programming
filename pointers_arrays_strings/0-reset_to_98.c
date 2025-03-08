a497090f60a5354f0bc83977f03106bd1c6dad12
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-08 05:54:03 +0000

    98

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,10 @@
 
 /**
  * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98
- * *n
+ *
  * Return: (0)
  */
 void reset_to_98(int *n)
 {
-		
+	*n = 98;	
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 int _putchar(char c);
 void reset_to_98(int *n);
-vid swap_int(int *a, int *b);
+void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
@@ -11,7 +11,7 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-void char *_strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
 #endif
