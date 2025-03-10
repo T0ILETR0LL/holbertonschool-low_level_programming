73e3119d2c03f1a79b269fa9d98347c78a0e79d7
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-10 06:24:53 +0000

    reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_rev- prints a string in reverse
+ * @s: variable
+ * Return: (0)
+ */
+void print_rev(char *s)
+{
+	int end;
+	end = _strlen(s);
+
+	while (end >= 0)
+	{
+	_putchar(s[end]);
+	end--;
+	}
+	_putchar('\n');
+}
+
+
+#include "main.h"
+
+/**
+ * _strlen- returns the length of a string
+ * @s: string
+ * Return: (length)
+ */
+int _strlen(char *s)
+{
+        int length;
+
+        length = 0;
+        while (s[length] != '\0')
+        {
+                length++;
+        }
+
+        return (length);
+}
