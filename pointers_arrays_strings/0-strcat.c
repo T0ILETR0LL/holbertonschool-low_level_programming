1fe80b38ada16b9265206190fa6db6a008b1f3d1
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-12 08:42:07 +0000

    strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - prints a string in reverse
+ * @s: variable
+ * Return: (0)
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+	a = 0;
+	b = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
