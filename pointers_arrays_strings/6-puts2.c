77e936892915f781d165e070f8a63ef588f37900
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-13 03:22:44 +0000

    changed

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,11 +7,13 @@
  */
 void puts2(char *str)
 {
-	while (*str < '\0')
+	while (*str != '\0')
 	{
-	_putchar(*str);
-	str++;
-	str++;
+		if (*str % 2 == 0)
+		{
+			_putchar(*str);
+		}
+		str++;
 	}
 	_putchar('\n');
 }
