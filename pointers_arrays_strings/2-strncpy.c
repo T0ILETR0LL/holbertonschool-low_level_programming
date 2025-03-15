692912bfb4bb8e5bdaf93796ef467e2d6d590a61
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-15 11:31:08 +0000

    changes

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy- copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n : number of characters
+ * Return: (dest)
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+	int b;
+
+	a = 0;
+	b = 0;
+
+
+	while (b < n && src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[n] = '\0';
+	return (dest);
+}
