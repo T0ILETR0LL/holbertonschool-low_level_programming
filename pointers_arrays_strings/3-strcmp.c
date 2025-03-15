901afc26fb528574d0f1ccf0c56e31fc8877d9c3
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-15 12:54:28 +0000

    compare

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -8,12 +8,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	if (s1 != s2)
+	
+	if (s1 == s2)
 	{
-		return(s1[0] - s2[0]);
+		return(0);
 	}
 	else
 	{
-		return (0);
+		return (s1[0] - s2[0]);
 	}
 }
