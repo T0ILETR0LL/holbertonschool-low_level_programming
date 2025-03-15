b110e7a9a2686e335da780e65470a0d6afb53b70
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-15 12:47:18 +0000

    compare

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -8,11 +8,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	if (s1[0] > s2[0])
+	if (s1 > s2)
 	{
 		return (s1[0] - s2[0]);
 	}
-	if (s1[0] < s2[0])
+	if (s1 < s2)
 	{
 		return(s1[0] - s2[0]);
 	}
