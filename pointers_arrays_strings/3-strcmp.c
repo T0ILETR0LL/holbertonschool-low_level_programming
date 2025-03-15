8096e8d15f6bb907a60dff0997eded73198f4578
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-15 13:20:14 +0000

    compare

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,8 +11,12 @@ int _strcmp(char *s1, char *s2)
 	int n;
 
 	n = 0;
-	while (s1[n] == s2[n] && n == '\0')
+	while (s1[n] == s2[n] && n != '\0')
 	{
+		if(s1[n] != s2[n])
+		{
+			return (s1[n] - s2[n]);
+		}
 		n++;
 	}
 	return (s1[n] - s2[n]);
