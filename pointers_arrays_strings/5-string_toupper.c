e9528a3766981cbf7e82259c76094ecdfbce3afc
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-16 04:28:23 +0000

    upper

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -3,7 +3,7 @@
 /**
  * string_toupper- changes all lowercase letters of a string to uppercase
  * @s: string
- * Return: (string
+ * Return: (s)
  */
 char *string_toupper(char *s)
 {
@@ -12,12 +12,12 @@ char *string_toupper(char *s)
 	n = 0;
 	while (s[n] != '\0')
 	{
-	
+
 		if ('a' <= s[n] && 'z' >= s[n])
 		{
 			s[n] = s[n] - 32;
 		}
 		n++;
 	}
-	return(s);
+	return (s);
 }
