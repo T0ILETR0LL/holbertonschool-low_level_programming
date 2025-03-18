1cdc81a82b9b8c31c6c517d81e463c18538bc862
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-18 10:57:15 +0000

    strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -9,31 +9,31 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int n;
-	int b;
-	int length;
-	int found;
+        int n;
+        int b;
+        int length;
+        int found;
 
-	length = 0;
-	n = 0;
-	while (s[n] != '\0')
-	{
-		found = 0;
-		b = 0;
-		while (accept[b] != '\0' && found == 0)
-		{
-			if (s[n] == accept[b])
-			{
-				length++;
-				found++;
-			}
-			b++;
-		}
-		if (found == 0)
-		{
-			return (length);
-		}
-		n++;
-	}
-	return (length);
+        length = 0;
+        n = 0;
+        while (s[n] != '\0')
+        {
+                found = 0;
+                b = 0;
+                while (accept[b] != '\0' && found == 0)
+                {
+                        if (s[n] == accept[b])
+                        {
+                                length++;
+                                found++;
+                        }
+                        b++;
+                }
+                if (found == 0)
+                {
+                        return (length);
+                }
+                n++;
+        }
+        return (length);
 }
