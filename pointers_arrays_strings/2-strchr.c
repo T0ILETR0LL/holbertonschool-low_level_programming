07daeb9a32cd1063b261e955c33f3246e036568f
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-18 04:12:53 +0000

    strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -14,11 +14,15 @@ char *_strchr(char *s, char c)
 	n = 0;
 	while (s[n] != '\0')
 	{
-		if (s[n] == c || c == '\0')
+		if (s[n] == c)
 		{
 			return (&s[n]);
 		}
 	n++;
+	}
+	if (s[n] == '\0' && c == '\0')
+	{
+		return (&s[n]);
 	}
 	return (NULL);
 }
