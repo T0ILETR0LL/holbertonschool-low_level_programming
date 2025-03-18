364caa4ee48ca045cacca966d89b8fb203a9d233
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-18 11:41:00 +0000

    needle

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr- locates a substring
+ * @s: string to scan
+ * @accept: string to match
+ * Return: (null) (&s)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int n;
+	int b;
+
+	n = 0;
+	b = 0;
+	while (haystack[n] != '\0')
+	{
+		if (needle[b] == haystack[n])	
+		{
+			while (needle[b] == haystack[n] && needle[b] != '\0')
+			{
+				b++;
+				n++;
+				return (&haystack[n - 1]);
+			}
+		}
+		n++;
+	}
+	return (NULL);
+}
