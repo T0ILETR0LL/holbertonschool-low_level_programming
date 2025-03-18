97636f64310d3d5e929adf61cb6077251f8cec29
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-18 12:11:53 +0000

    needles

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -11,18 +11,19 @@ char *_strstr(char *haystack, char *needle)
 {
 	int n;
 	int b;
-	int locate;
 
 	n = 0;
 	while (haystack[n] != '\0')
 	{
 		b = 0;
-		locate = n;
-		if (needle[b] == haystack[locate] && needle[b] != '\0')
+		while(needle[b] == haystack[n])
 		{
 			b++;
-			locate++;
-			return (&haystack[locate]);
+			n++;
+		}
+		if (needle [b] == '\0')
+		{
+			return (&haystack[n]);
 		}
 		n++;
 	}
