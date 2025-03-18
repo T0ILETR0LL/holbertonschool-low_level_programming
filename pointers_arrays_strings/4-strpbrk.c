b68cf31893a8bf18d97f4031991ef9e3249ec827
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-18 11:17:50 +0000

    strpbrk

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk- legnth of a prefix substring
+ * @s: string to scan
+ * @accept: string to match
+ * Return: (0)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int n;
+	int b;
+
+	n = 0;
+	while (s[n] != '\0')
+	{
+		b = 0;
+		while (accept [b] != '\0')
+		{
+			if (s[n] == accept[b])
+			{
+			return (&s[n]);
+			}
+			b++;
+		}
+		n++;
+	}
+	return (NULL);
+}
