dc73fd42cacd3e8f86c23ce0be732a23f734d740
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-18 03:40:13 +0000

    strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr- locates a character in a string
+ * @s: string
+ * @c: character
+ * Return: (0)
+ */
+char *_strchr(char *s, char c)
+{
+	int n;
+
+	n = 0;
+	while (s[n] != '\0')
+	{
+		if (s[n] == c)
+		{
+			return (&s[n]);
+		}
+	n++;
+	}
+	return (NULL);
+}
