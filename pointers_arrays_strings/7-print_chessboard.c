9b5ca08b8fe9abad4ef4a211dd8f2a87ca95071c
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-18 12:35:58 +0000

    chess

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - prints a chessboard
+ * @a: string
+ * Return: (0)
+ */
+void print_chessboard(char (*a)[8])
+{
+	int n;
+	int b;
+
+	n = 0;
+	while (n < 8)
+	{
+		b = 0;
+		while(b < 8)
+		{
+			_putchar(a[n][b]);
+			b++;
+		}
+		_putchar('\n');
+		n++;
+	}
+}
