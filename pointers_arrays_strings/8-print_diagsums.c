a0d689c099936af4abc3636bd9cb8fc774767d68
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-18 14:26:16 +0000

    sleep

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - sum of two diagonals
+ * @a: string
+ * @size: length of square matrix
+ * Return: (0)
+ */
+void print_diagsums(int *a, int size)
+{
+	int b;
+	int c;
+	int n;
+	int m;
+	int diagonalone;
+	int diagonaltwo;
+
+	n = size + 1;
+	diagonalone = 0;
+	b = 0;
+	while (b < size)
+	{
+		diagonalone += a[n*b];
+		b++;
+	}
+	diagonaltwo = 0;
+	m = size - 1;
+	c = 1;
+	while (c <= size)
+	{
+		diagonaltwo += a[m*c];
+		c++;
+	}
+	printf ("%i, %i\n", diagonalone, diagonaltwo);
+}
