3f77b41434783829875510992356152058956711
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-19 12:50:06 +0000

    new

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string
+ * Return: (0)
+ */
+void _puts_recursion(char *s)
+{
+	if (s == '\0')
+	{
+		return (0);
+	}
+	return (s);
+}
