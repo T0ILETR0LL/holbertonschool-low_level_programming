c9ad1b9761af0c3bae73f47939114ce8c6537bff
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-20 02:00:47 +0000

    reverse

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev_recursion- prints a string followed by a new line
+ * @s: string
+ * Return: (0) (s)
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '0')
+	{
+		_putchar(*s);
+		_print_rev_recursion(s + 1);
+		return;
+	}
+}
