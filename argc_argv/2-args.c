8557261cf3735e27375f75d0f37bc4d4bc235ee0
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-21 11:59:02 +0000

    oneline

---
+++ argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main- prints all arguments it recieves
+ * @argc: number of arguments
+ * @argv: string
+ * Return: (0)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	i = 0;
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
