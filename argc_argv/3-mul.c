ecc851f8cf196b61a1d6d36c7c5ef68bf7be21c3
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-21 12:34:21 +0000

    multiply

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main- multiplies two numbers
+ * @argc: number of arguments
+ * @argv: string
+ * Return: (0)
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+	(void)argv;
+
+	if (argc == 1)
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+	result = (argc) * (argc - 1);
+	printf("%d\n", result);
+	
+	return (0);
+}
