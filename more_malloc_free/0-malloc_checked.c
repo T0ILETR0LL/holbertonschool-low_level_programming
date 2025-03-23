a7abb70d23fec942195a9e9d18cab482ba047dc6
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-23 12:17:58 +0000

    check

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -4,3 +4,18 @@
 #include <limits.h>
 
 /**
+ **malloc_checked- allocates memory using malloc
+ *@b: unsigned int
+ *Return: (pointer)
+ */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return(ptr);
+}
