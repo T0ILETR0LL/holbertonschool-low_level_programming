6f1ae5545e6059ab8ec6b9446608d05b09c744cd
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-23 03:12:25 +0000

    array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -4,17 +4,25 @@
 
 /**
  **create_array- create array of character
- *@size: 
+ *@size: size of array
  *@c: characters
  *Return: (0)
  */
 char *create_array(unsigned int size, char c)
 {
-	if (size == 0)
+	unsigned int n;
+	char *array;
+
+	n = 0;
+	array = malloc (size * sizeof(char));
+	if (size == 0 || array == NULL)
 	{
 		return (NULL);
 	}
-	*c = malloc(size * c)
-	
-	return (*c);
+	while (n < size)
+	{
+		array[n] = c;
+		n++;
+	}
+	return (array);
 }
