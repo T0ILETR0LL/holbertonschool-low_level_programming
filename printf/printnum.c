c50ed971eda562d17a3848bcf299791a081604b3
T0ILETR0LL <marianne.endozo@gmail.com>
2025-03-29 15:28:30 +0000

    A

--- printf/printnum.c
+++ printf/printnum.c
@@ -3,27 +3,30 @@
 
 int printnum (int n)
 {
-        int num = 0;
+        int num;
         int remainder = 0;
 
-        if (n == 0)
-        {
-                _putchar('0');
-                return(0);
-        }
+        
         if (n < 0)
         {
                 _putchar('-');
+		_putchar(n);
                 num = -n;
         }
         if (n > 0)
         {
                 num = n;
         }
+	if (num > 0)
+	{
+		_putchar('A');
+	}
+	/**
         if (num / 10)
         {
                 remainder += printnum(num % 10);
         }
-	_putchar(remainder);
+	*/
         return (remainder);
+
 }
