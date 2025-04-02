2bae1f85c4a48348390d8be59a7c0c4eca52fff3
T0ILETR0LL <marianne.endozo@gmail.com>
2025-04-02 23:56:32 +0000

    task 0 changes

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -21,8 +21,7 @@ size_t print_list(const list_t *h)
 		printf("[%d] %s\n", h->len, h->str); /*prints strings in every node */
 		}
 	n++;
-	h = h->next; 
+	h = h->next; /*syntax to access next node*/
 	}
-	printf("-> %d elements\n", n);
 	return(n);
 }
