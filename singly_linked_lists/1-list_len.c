6c91579c6d954fceb4e8dc2c65b406064912bf63
T0ILETR0LL <marianne.endozo@gmail.com>
2025-04-03 00:28:01 +0000

    task 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * list_len - prints the nodes
+ * @str: string - (malloc'ed string)
+ * @Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	int n = 0; /*counter for nodes*/
+	
+	while (h != NULL) /*nodes are running*/
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+		printf("[%d] %s\n", h->len, h->str); /*prints strings in every node */
+		}
+	n++;
+	h = h->next; /*syntax to access next node*/
+	}
+	return (n);
+}
