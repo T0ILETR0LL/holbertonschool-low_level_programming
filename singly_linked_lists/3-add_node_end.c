f6dfa5211a268d3c1fbd50bf7f746f5c3daadd58
T0ILETR0LL <marianne.endozo@gmail.com>
2025-04-03 06:50:50 +0000

    task 3

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *add_node_end - add node to the end
+ * @str: string - (malloc'ed string)
+ * @Return: address of new string
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new; /*new node*/
+	int i = 0; /* for string length */ 
+
+	new = malloc(sizeof(list_t)); /*allocate space for new node*/
+	if (new == NULL)
+	{
+		free(new);
+		return(NULL);
+	}
+	while (str[i] != '\0') 
+	{
+		i++;
+	}
+	new-> str = strdup(str);
+	new-> len = i;
+	new-> next = *head; /* remove this*/ 
+	*head = new;
+	return (new);
+	free(new);
+	/*if the end of string is null, insert node*/
+}
