3f8b1cff61f876077ba36a175887559a3a25a7fa
T0ILETR0LL <marianne.endozo@gmail.com>
2025-04-09 13:30:31 +0000

    structs

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * struct dog - new type
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog *d;
+ 	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+}
