05266f1ed8db50263caecd84b0c5c70fc97a547f
T0ILETR0LL <marianne.endozo@gmail.com>
2025-04-10 03:05:47 +0000

    changes

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * new_dog - initialize struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if 
+	{
+		return (NULL);
+	}
+} 
